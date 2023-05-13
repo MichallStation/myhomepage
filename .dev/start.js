72bc10f0f8a704f84609df37c9c7eea6ef473712
ltndat <ltndat@outlook.com>
2023-05-13 20:06:17 +0700

    add: works post english

--- .dev/start.js
+++ .dev/start.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '..')));
 
