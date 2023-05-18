04108275545e726873fa493261f2c098b8ae9d8b
ltndat <ltndat@outlook.com>
2023-05-18 15:27:00 +0700

    update: something news ✒

--- .dev/dev.js
+++ .dev/dev.js
@@ -7,8 +7,15 @@ const app = express();
 const port = 5000;
 
 app.use(cors());
+app.use(express.json());
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '..')));
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  console.log(req.headers);
+  res.status(200).json(req.body);
+});
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
